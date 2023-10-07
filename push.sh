b248c60333988bd169d999fcd8cccf9ad749caa5
GX530381 <1813515674@qq.com>
2023-10-07 10:34:51 +0800

    xxx

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'GX530381'
 git config --global user.email '1813515674@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m 
 git push
